7efda1b1cad643aa247913216b92d23c4569c9c8
jmservera <jmservera@outlook.com>
2020-02-21 21:00:20 +0100

    simpler message

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
-const http = require('http');
-const WebSocketServer = require('websocket').server;
+const ws = require('ws');
+const wsServer = new ws.Server({port:process.env.SERVER_PORT||8081});
 
-const server = http.createServer(function(request,response){
-    console.log((new Date()) + ' Received ws request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-} );
-server.listen(process.env.SERVER_PORT||8081,function(){
-    console.log(`Listening on ${server.address().address}${server.address().port}`);
-});
-
-const wsServer = new WebSocketServer({
-    httpServer: server
-});
-
-wsServer.on('request', function(request) {
-    const connection = request.accept(null, request.origin);
-    Console.log('WS Request received.')
+console.log(`Listening on ${wsServer.address().address}${wsServer.address().port}`);
+wsServer.on('connection', function(connection) {
+    console.log('WS Request received.')
 
     connection.on('message', function(message) {
-      console.log('Received ws Message:', message.utf8Data);
-      connection.sendUTF('Hello I\'m the server!');
+        console.log('Received ws Message:', message);
+        connection.send('Hello I\'m the server!');
     });
     connection.on('close', function(reasonCode, description) {
         console.log('Client has disconnected.');
